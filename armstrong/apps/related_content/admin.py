007cd14cd3fd215cd91403ebe09cd5c0bb555f23
Travis Swicegood <development@domain51.com>
2011-08-19 17:12:01 -0500

    Add in visualsearch for GFK

--- armstrong/apps/related_content/admin.py
+++ armstrong/apps/related_content/admin.py
@@ -1,8 +1,22 @@
+from django import forms
 from django.contrib import admin
 from django.contrib.contenttypes.generic import GenericTabularInline
 
+from armstrong.hatband import widgets
+
 from .models import RelatedContent
 from .models import RelatedType
+
+class RelatedContentInlineForm(forms.ModelForm):
+    class Meta:
+        widgets = {
+            "destination_type": forms.HiddenInput(),
+            "destination_id": widgets.GenericKeyWidget(
+                object_id_name="destination_id",
+                content_type_name="destination_type",
+            ),
+            "order": forms.HiddenInput(),
+        }
 
 
 class RelatedContentInline(GenericTabularInline):
@@ -10,6 +24,9 @@ class RelatedContentInline(GenericTabularInline):
     ct_fk_field = "source_id"
 
     model = RelatedContent
+    template = "admin/edit_inline/generickey.html"
+
+    form = RelatedContentInlineForm
 
 
 admin.site.register(RelatedType)
