50e6ffcef5a7220a2c59f3cb9dc9793fa16f00f0
Travis Swicegood <development@domain51.com>
2011-12-15 15:16:22 -0600

    pep8ify

--- armstrong/apps/related_content/tests/related_content_support/models.py
+++ armstrong/apps/related_content/tests/related_content_support/models.py
@@ -7,12 +7,16 @@ class Article(models.Model):
     title = models.CharField(max_length=250)
     related = RelatedContentField()
 
+
 class TestingThumbnailMixin(BaseThumbnailMixin):
-    def render_visual(self, preset_label, presets=None, defaults=None, *args, **kwargs):
+    def render_visual(self, preset_label, presets=None, defaults=None,
+            *args, **kwargs):
         return "Render: %s" % preset_label
 
-    def get_visual_thumbnail_url(self, preset_label, presets=None, defaults=None, *args, **kwargs):
+    def get_visual_thumbnail_url(self, preset_label, presets=None,
+            defaults=None, *args, **kwargs):
         return "Thumb: %s" % preset_label
+
 
 class Image(models.Model, TestingThumbnailMixin):
     title = models.CharField(max_length=250)
