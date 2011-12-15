19f47ac932fc91dcc1ccfdd80013dc52b998c491
Travis Swicegood <development@domain51.com>
2011-12-15 13:50:30 -0600

    Remove duplication in tests

--- armstrong/apps/related_content/tests/managers.py
+++ armstrong/apps/related_content/tests/managers.py
@@ -11,22 +11,30 @@ def relate(type, source, destination):
         destination_object=destination)
 
 
+def create_one_source_to_two_destinations():
+    one, two, three = generate_fake_articles(3)
+    t = RelatedType.objects.create(title="articles")
+    relate(t, one, two)
+    relate(t, one, three)
+    return one, two, three
+
+
+def create_two_sources_to_one_destination():
+    one, two, three = generate_fake_articles(3)
+    t = RelatedType.objects.create(title="articles")
+    relate(t, one, two)
+    relate(t, three, two)
+    return one, two, three
+
+
 class RelatedContentManagerTestCase(TestCase):
     def test_can_find_by_destination_object(self):
-        one, two, three = generate_fake_articles(3)
-        t = RelatedType.objects.create(title="articles")
-        relate(t, one, two)
-        relate(t, one, three)
-
+        one, two, three = create_one_source_to_two_destinations()
         related_content = RelatedContent.objects.filter(destination_object=two)
         self.assertEqual(1, related_content.count())
 
     def test_can_find_by_source_object(self):
-        one, two, three = generate_fake_articles(3)
-        t = RelatedType.objects.create(title="articles")
-        relate(t, one, two)
-        relate(t, three, two)
-
+        one, two, three = create_two_sources_to_one_destination()
         related_content = RelatedContent.objects.filter(source_object=one)
         self.assertEqual(1, related_content.count())
 
@@ -46,31 +54,19 @@ class RelatedContentManagerTestCase(TestCase):
         self.assertTrue(two in destination_objects)
 
     def test_uses_queryset_with_custom_filter(self):
-        one, two, three = generate_fake_articles(3)
-        t = RelatedType.objects.create(title="articles")
-        relate(t, one, two)
-        relate(t, one, three)
-
+        one, two, three = create_one_source_to_two_destinations()
         related_content = (RelatedContent.objects.all()
                 .filter(destination_object=two))
         self.assertEqual(1, related_content.count())
 
     def test_provides_method_for_finding_by_destination(self):
-        one, two, three = generate_fake_articles(3)
-        t = RelatedType.objects.create(title="articles")
-        relate(t, one, two)
-        relate(t, one, three)
-
+        one, two, three = create_one_source_to_two_destinations()
         related_content = RelatedContent.objects.by_destination(two)
         self.assertEqual(1, related_content.count())
         self.assertEqual(two, related_content[0].destination_object)
 
     def test_provides_method_for_finding_by_source(self):
-        one, two, three = generate_fake_articles(3)
-        t = RelatedType.objects.create(title="articles")
-        relate(t, one, two)
-        relate(t, three, two)
-
+        one, two, three = create_two_sources_to_one_destination()
         related_content = RelatedContent.objects.by_source(one)
         self.assertEqual(1, related_content.count())
         self.assertEqual(one, related_content[0].source_object)
