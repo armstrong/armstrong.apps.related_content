a10d52d6068f92c1e0e81e2a425567a7fafb14e6
Travis Swicegood <development@domain51.com>
2011-12-15 13:15:43 -0600

    Tweak filter() to allow `source_object` and `destination_object`
    This makes it possible to search for a source or destination object
    directly via the object instead of having to pass its ID and
    corresponding `ContentType` object as parameters.

--- armstrong/apps/related_content/managers.py
+++ armstrong/apps/related_content/managers.py
@@ -1,6 +1,22 @@
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
+
+get_for_model = ContentType.objects.get_for_model
 
 
 class RelatedContentManager(models.Manager):
+    def filter(self, destination_object=None, source_object=None, **kwargs):
+        if destination_object:
+            kwargs.update({
+                "destination_id": destination_object.pk,
+                "destination_type": get_for_model(destination_object),
+            })
+        if source_object:
+            kwargs.update({
+                "source_id": source_object.pk,
+                "source_type": get_for_model(source_object),
+            })
+        return super(RelatedContentManager, self).filter(**kwargs)
+
     def by_type(self, type):
         return self.filter(related_type__title=type)
--- armstrong/apps/related_content/tests/managers.py
+++ armstrong/apps/related_content/tests/managers.py
@@ -4,7 +4,32 @@ from ..models import RelatedContent
 from ..models import RelatedType
 
 
+def relate(type, source, destination):
+    return RelatedContent.objects.create(
+        related_type=type,
+        source_object=source,
+        destination_object=destination)
+
+
 class RelatedContentManagerTestCase(TestCase):
+    def test_can_find_by_destination_object(self):
+        one, two, three = generate_fake_articles(3)
+        t = RelatedType.objects.create(title="articles")
+        relate(t, one, two)
+        relate(t, one, three)
+
+        related_content = RelatedContent.objects.filter(destination_object=two)
+        self.assertEqual(1, related_content.count())
+
+    def test_can_find_by_source_object(self):
+        one, two, three = generate_fake_articles(3)
+        t = RelatedType.objects.create(title="articles")
+        relate(t, one, two)
+        relate(t, three, two)
+
+        related_content = RelatedContent.objects.filter(source_object=one)
+        self.assertEqual(1, related_content.count())
+
     def test_by_type_returns_an_empty_queryset_if_no_match_is_found(self):
         one, two = generate_fake_articles(2)
 
