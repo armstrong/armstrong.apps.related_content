5b802893833002db4488ea42320c28411c83e0f5
Dave McLain <dmclain@texastribune.org>
2012-03-01 11:58:28 -0600

    more tests

--- armstrong/apps/related_content/tests/fields.py
+++ armstrong/apps/related_content/tests/fields.py
@@ -2,6 +2,7 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.generic import GenericForeignKey
 from django.db import models
 from ._utils import *
+from django import template
 
 from ..models import RelatedContent
 from ..models import RelatedType
@@ -22,6 +23,31 @@ class RelatedObjectsFieldTestCase(TestCase):
         related_content = one.related.all()
         self.assertEqual(1, related_content.count())
         self.assertEqual(related_content[0], two)
+
+    def test_filtering_by_type(self):
+        one, two, c = generate_model()
+        related_content = one.related.by_type(c.related_type.title)
+        self.assertEqual(1, related_content.count())
+        self.assertEqual(related_content[0], two)
+
+    def test_filtering_by_getitem(self):
+        one, two, c = generate_model()
+        related_content = one.related[c.related_type.title]
+        self.assertEqual(1, related_content.count())
+        self.assertEqual(related_content[0], two)
+
+    def test_filtering_by_type_with_no_results(self):
+        one, two, c = generate_model()
+        related_content = one.related.by_type("invalid_type")
+        self.assertEqual(0, related_content.count())
+
+    def test_template_filtering(self):
+        one, two, c = generate_model()
+        c.related_type.title = "type_title"
+        c.related_type.save()
+        t = template.Template("{{ one.related.type_title.0.title }}")
+        result = t.render(template.Context(locals()))
+        self.assertEqual(result, two.title)
 
 
 class ReverseRelatedObjectsFieldTestCase(TestCase):
